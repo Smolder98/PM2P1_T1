4658d99f9ff9882cd4d8452a336c89e087c558b3
Smolder98 <alicoescobar@gmail.com>
2022-06-19 23:25:08 -0600

    Add project files.

---
+++ PM2P1_T1/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PM2P1_T1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Operation_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(entryFirstNumber.Text) || string.IsNullOrWhiteSpace(entrySecondNumber.Text))
+            {
+                message("Alerta", "Debe escribir ambos numeros");
+                return;
+            }
+
+            decimal num1 = decimal.Parse(entryFirstNumber.Text);
+            decimal num2 = decimal.Parse(entrySecondNumber.Text);
+            decimal result;
+
+
+            if (sender.Equals(btnSum))
+            {
+                result = num1 + num2;
+            }
+            else if (sender.Equals(btnRest))
+            {
+                result = num1 - num2;
+            }
+            else if (sender.Equals(btnDiv))
+            {
+                if (num2 == 0)
+                {
+                    message("Alerta", "El segundo numero no puede ser 0");
+                    return;
+                }
+                result = num1 / num2;
+            }
+            else 
+            {
+                result = num1 * num2;
+            }
+            await Navigation.PushAsync(new ResultPage(result));
+        }
+
+        private async void message(string title, string description)
+        {
+            await DisplayAlert(title, description, "OK");
+        }
+
+        private void btnClear_Clicked(object sender, EventArgs e)
+        {
+            entryFirstNumber.Text = "";
+            entrySecondNumber.Text = "";
+        }
+    }
+}
---
+++ PM2P1_T1/ResultPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM2P1_T1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ResultPage : ContentPage
+    {
+        public ResultPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        public ResultPage(decimal result)
+        {
+            InitializeComponent();
+
+            txtResult.Text = "El resultado es :" + result.ToString("0.##");
+        }
+    }
+}
